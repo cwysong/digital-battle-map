5c494340a3eb4c5efd690e11cf2996a885f726cd
cwysong <corbin.wysong@gmail.com>
2024-06-14 22:46:24 -0700

    Thats a lot of changes for one module

--- UI.py
+++ UI.py
@@ -2,26 +2,44 @@ import tkinter
 from tkinter import messagebox
 import cell
 from elements import *
+import customtkinter
 
 # UI implementation from tutorial
 class GUI:
 
-    def __init__(self):
+    def __init__(self, width:int , height:int, title:str):
         self.ui = tkinter.Tk()
+        
+        self.menubar = tkinter.Menu(self.ui)
+
+        self.filemenu = tkinter.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Close", command=self.on_close)
+
+        self.actionmenu = tkinter.Menu(self.menubar, tearoff=0)
+        
+        # add commands
+        self.actionmenu.add_command(label = "Add Entity", command = None)
+        self.actionmenu.add_separator()
+        self.actionmenu.add_command(label = "Add Background", command = None)
+
+        self.menubar.add_cascade(menu=self.filemenu, label="File")
+        self.menubar.add_cascade(menu=self.actionmenu, label="Actions")
+
+        self.ui.config(menu=self.menubar)
 
         self.effects = status_effect()
 
-        # self.ui.geometry("1200x800")
-        self.ui.geometry("500x500")
-        self.ui.title("Battle Mat")
+        self.ui.geometry(f"{width}x{height}")
+        self.ui.title(title)
 
-        self.label = tkinter.Label(self.ui, text = "Battle", font = ('Arial', 16))
+        self.label = tkinter.Label(self.ui, text = title, font = ('Arial', 16))
         self.label.pack(padx=10, pady=10)
 
         self.textbox = tkinter.Text(self.ui, height = 5, font = ('Arial', 16))
         self.textbox.bind("<KeyPress>", self.check_effects)
         self.textbox.pack(padx=10, pady=10)
 
+        self.ui.protocol("WM_DELETE_WINDOW", self.on_close)
         self.ui.mainloop()
 
     def check_cell(self, row, col):
@@ -33,3 +51,8 @@ class GUI:
         effect = "prone"
         if event.state == 4 and event.keysym == "s":
             messagebox.showinfo(title = effect, message = self.effects.define(effect))
+
+    def on_close(self):
+        if messagebox.askyesno(title="Quit?", message = "You A Loser?"):
+            self.ui.destroy()
+        
--- digital-battle-map.py
+++ digital-battle-map.py
@@ -4,6 +4,7 @@ from ui import *
 class run():
     
     def __init__(self) -> None:
-        self.gui = GUI()
+        # in parenthesis type the desired height and width separated by a comma, and the title in the Quotes after a comma
+        self.gui = GUI(500, 500, "Battle Mat")
 
 run()
