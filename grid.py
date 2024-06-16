8e51559810bdd286fb44c880b3cafdd41bcc7fe0
cwysong <corbin.wysong@gmail.com>
2024-06-15 17:24:50 -0700

    Scrolling and selecting now works

--- grid.py
+++ grid.py
@@ -3,7 +3,9 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 
 # Constants for the grid size
-CELL_SIZE = 50  # Size of each cell in pixels
+CELL_SIZE = 60  # Size of each cell in pixels
+SCROLL_AMOUNT = 20  # Amount to scroll on each key press
+
 
 class DndBattleMap(tk.Frame):
     def __init__(self, master=None):
@@ -11,6 +13,7 @@ class DndBattleMap(tk.Frame):
         self.master = master
         self.grid()
         self.create_widgets()
+        self.setup_scroll_bindings()
 
     def create_widgets(self):
         self.background_image = Image.open("cave.jpg")  # Replace with your image file path
@@ -21,22 +24,21 @@ class DndBattleMap(tk.Frame):
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.background_photo)
         self.canvas.grid()
 
-        self.lastClickX = 0
-        self.lastClickY = 0
+        # self.lastClickX = 0
+        # self.lastClickY = 0
 
-        self.master.overrideredirect(True)
-        self.master.attributes('-topmost', True)
-        self.canvas.bind('<Button-1>', self.SaveLastClickPos)
-        self.canvas.bind('<B1-Motion>', self.Dragging)
+        # self.master.overrideredirect(True)
+        # self.master.attributes('-topmost', True)
+        # self.canvas.bind('<Button-3>', self.SaveLastClickPos)
+        # self.canvas.bind('<B3-Motion>', self.Dragging)
+        self.canvas.bind('<Button-1>', self.left_click)
+        self.canvas.bind('<Button-2>', self.middle_click)
 
         # Create grid lines
         for i in range(self.background_image.size[0]):
             x = i * CELL_SIZE
             self.canvas.create_line(x, 0, x, self.background_image.size[0] * CELL_SIZE)
             self.canvas.create_line(0, x, self.background_image.size[0] * CELL_SIZE, x)
-
-        # Example: Draw a player token
-        self.draw_token(2, 3, 'blue')  # Example position (2, 3) with blue color
 
         self.menubar = tk.Menu(self.master)
 
@@ -55,29 +57,62 @@ class DndBattleMap(tk.Frame):
 
         self.master.config(menu=self.menubar)
 
-    def draw_token(self, x, y, color):
-        # Draw a token at grid position (x, y)
-        x_pix = x * CELL_SIZE
-        y_pix = y * CELL_SIZE
-        self.canvas.create_oval(x_pix, y_pix, x_pix + CELL_SIZE, y_pix + CELL_SIZE, fill=color)
-
     def on_close(self):
         if messagebox.askyesno(title="Quit?", message = "You A Loser?"):
             self.master.destroy()
 
-    def SaveLastClickPos(self, event):
-        self.lastClickX = event.x
-        self.lastClickY = event.y
+    # def SaveLastClickPos(self, event):
+    #     self.lastClickX = event.x
+    #     self.lastClickY = event.y
 
 
-    def Dragging(self, event):
-        x, y = event.x - self.lastClickX + self.master.winfo_x(), event.y - self.lastClickY + self.master.winfo_y()
-        self.master.geometry("+%s+%s" % (x , y))
+    # def Dragging(self, event):
+    #     x, y = event.x - self.lastClickX + self.master.winfo_x(), event.y - self.lastClickY + self.master.winfo_y()
+    #     self.master.geometry("+%s+%s" % (x , y))
+
+    def left_click(self, event):
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        cell_left = round(x/CELL_SIZE)*CELL_SIZE
+        cell_right = cell_left+CELL_SIZE
+        cell_up = round(y/CELL_SIZE)*CELL_SIZE
+        cell_down = cell_up+CELL_SIZE
+        self.canvas.create_rectangle(cell_left, cell_up, cell_right, cell_down, outline='white')
+
+    def middle_click(self, event):
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        cell_left = round(x/CELL_SIZE)*CELL_SIZE
+        cell_right = cell_left+CELL_SIZE
+        cell_up = round(y/CELL_SIZE)*CELL_SIZE
+        cell_down = cell_up+CELL_SIZE
+        self.canvas.create_rectangle(cell_left, cell_up, cell_right, cell_down, outline='black')
+
+    def setup_scroll_bindings(self):
+        self.canvas.bind("<KeyPress-w>", self.scroll_up)
+        self.canvas.bind("<KeyPress-a>", self.scroll_left)
+        self.canvas.bind("<KeyPress-s>", self.scroll_down)
+        self.canvas.bind("<KeyPress-d>", self.scroll_right)
+
+        self.canvas.focus_set()  # Set focus to the canvas to capture key events
+
+    def scroll_up(self, event):
+        self.canvas.yview_scroll(-1, "units")
+
+    def scroll_down(self, event):
+        self.canvas.yview_scroll(1, "units")
+
+    def scroll_left(self, event):
+        self.canvas.xview_scroll(-1, "units")
+
+    def scroll_right(self, event):
+        self.canvas.xview_scroll(1, "units")
 
 def main():
     root = tk.Tk()
     root.title("DND Battle Map")
     app = DndBattleMap(master=root)
+    app.master.protocol("WM_DELETE_WINDOW", app.on_close)
     app.mainloop()
 
 main()
