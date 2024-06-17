b5f8b976c53142de38009fa9fa2181f24bf1bff5
cwysong <corbin.wysong@gmail.com>
2024-06-16 23:21:58 -0700

    Working prototype python and html

--- cell.py
+++
@@ -1,9 +0,0 @@
-from elements import entity
-
-class cell: 
-    
-    def __init__(self) -> None:
-        self.creature = None
-
-    def add_entity(self, creature:entity):
-        self.creature = creature
